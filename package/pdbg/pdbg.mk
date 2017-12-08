f6a6f295afac680e04b941b06072b956e34fb113
Joel Stanley <joel@jms.id.au>
2017-12-08 14:18:03 +1030

    pdbg: Bump version
    Contains the upstreamed build fix for Blackfin and various other bug
    fixes.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- package/pdbg/pdbg.mk
+++ package/pdbg/pdbg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PDBG_VERSION = 527fe54e1c95e031efd3db008c3f4e096cb3ea58
+PDBG_VERSION = 42763c33c8903ef3c26171ed4d148374296512d2
 PDBG_SITE = $(call github,open-power,pdbg,$(PDBG_VERSION))
 PDBG_LICENSE = Apache-2.0
 PDBG_LICENSE_FILES = COPYING
